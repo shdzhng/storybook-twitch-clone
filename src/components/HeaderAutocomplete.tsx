68599bacd2f206f0bb47cdf5b6d263613c3b196e
Shou Zhang <shoud.zhang@gmail.com>
2022-09-28 12:33:39 -0700

    add boilerplate for header autocomplete

---
+++ src/components/HeaderAutocomplete.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface HeaderAutocompleteProps {}
+
+const HeaderAutocomplete: React.FC<HeaderAutocompleteProps> = () => {
+  return <div>HeaderAutocomplete</div>;
+};
+
+export { HeaderAutocomplete };
---
+++ src/stories/HeaderAutocomplete.stories.tsx
@@ -0,0 +1,1 @@
+import { HeaderAutocomplete } from '../components/HeaderAutocomplete';
